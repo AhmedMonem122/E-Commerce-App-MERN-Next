34a2be5af22f975e2a0f731b4045f272c6f1c6c1
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-05 17:22:31 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+const Footer = () => {
+  return (
+    <footer className="px-7 md:px-40 py-10 text-[#4A739C]">
+      <ul className="flex justify-between items-center gap-4 flex-wrap">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Products</a>
+        </li>
+        <li>
+          <a href="">Brands</a>
+        </li>
+        <li>
+          <a href="">Categories</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+      </ul>
+      <p className="text-center mt-6">
+        &copy;2026 Ahmed Monem. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
