dfdf1b23a9d16c71c5569bc8c240d75e2a99ff95
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-12 19:09:19 +0300

    feat: add best deals to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BestDeals from "@/components/BestDeals/BestDeals";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BestDeals />
     </>
   );
 }
