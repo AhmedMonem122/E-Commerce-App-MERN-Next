548aca32447397e30e4715484b704a6074b8f777
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-12 17:51:49 +0300

    feat: add axios api base url config

---
+++ src/api/axios.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+// Interceptor to dynamically set token before each request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      delete config.headers.Authorization;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
