da86e2e3ba2742d003d36eb1e0100a20ce421de7
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-18 13:57:14 +0300

    feat: add tanstack query context provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import TanstackQueryContextProvider from "@/context/TanstackQueryContextProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F7FAFC] text-[#0D141C]`}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <TanstackQueryContextProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </TanstackQueryContextProvider>
       </body>
     </html>
   );
