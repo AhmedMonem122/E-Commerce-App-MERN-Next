b5b2fb73aba261abf60e6c0d5da3dee6a92d39f1
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-18 13:01:21 +0300

    feat: add home categories to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import BestDeals from "@/components/BestDeals/BestDeals";
 import Header from "@/components/Header/Header";
+import HomeCategories from "@/components/HomeCategories/HomeCategories";
 
 export default function Home() {
   return (
     <>
       <Header />
       <BestDeals />
+      <HomeCategories />
     </>
   );
 }
