3719d5347b7a7d53ed9735258f5d2a847c1bf78a
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-18 13:56:14 +0300

    feat: add product model

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -12,3 +12,15 @@ export interface TopCheapProduct {
   ratingsAverage: number;
   id: string;
 }
+
+export interface Product {
+  _id: string;
+  id: string;
+  title: string;
+  images: string[];
+  imageCover: string;
+  description: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  price: number;
+}
