ce7982e68c5b7163e6948c99aac9942234f24929
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-10-25 16:36:02 +0300

    feat: add brand model

---
+++ src/types/brand.d.ts
@@ -0,0 +1,10 @@
+export interface Brand {
+  _id: string;
+  title: string;
+  createdAt: string;
+  image: string;
+  updatedAt: string;
+  description: string;
+  products: string[];
+  category: string;
+}
