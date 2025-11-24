20eb0eaad209c4e35360ece6bf0fd26cc29d3a60
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-11-24 14:18:58 +0200

    feat!: update delete review form component logic

--- src/components/ProductReviews/DeleteReviewForm.tsx
+++ src/components/ProductReviews/DeleteReviewForm.tsx
@@ -1,6 +1,13 @@
-import { deleteReviewAction } from "@/actions/reviewsActions/reviewsActions";
+"use client";
+
+import { useActionState, useEffect } from "react";
+import {
+  deleteReviewAction,
+  ActionResponse,
+} from "@/actions/reviewsActions/reviewsActions";
 import { Button } from "../ui/button";
 import { Review } from "@/types/productDetails";
+import { toast } from "sonner";
 
 const DeleteReviewForm = ({
   productId,
@@ -9,10 +16,18 @@ const DeleteReviewForm = ({
   productId: string;
   review: Review;
 }) => {
-  const deleteReviewActionWithId = deleteReviewAction.bind(null, productId);
+  const initialState: ActionResponse = {};
+
+  const actionWithId = deleteReviewAction.bind(null, productId);
+  const [state, formAction] = useActionState(actionWithId, initialState);
+
+  useEffect(() => {
+    if (state?.success) toast.success(state.message);
+    if (state?.success === false) toast.error(state.message);
+  }, [state]);
 
   return (
-    <form action={deleteReviewActionWithId}>
+    <form action={formAction}>
       <input type="hidden" name="reviewId" value={review._id} />
       <Button variant="destructive" size="sm" type="submit">
         Delete
