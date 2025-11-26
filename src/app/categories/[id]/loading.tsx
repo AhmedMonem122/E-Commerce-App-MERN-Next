2c92cf1d7641b5be74aff1393147268b3ea8b70d
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-11-26 16:09:10 +0200

    feat: add loading to category products page

---
+++ src/app/categories/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import CategoryBrandProductsSkeleton from "@/pages/CategoryBrandProducts/CategoryBrandProductsSkeleton";
+
+export default function Loading() {
+  return (
+    <main className="container mx-auto py-8">
+      <CategoryBrandProductsSkeleton />
+    </main>
+  );
+}
