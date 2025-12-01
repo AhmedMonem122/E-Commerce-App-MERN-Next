51602bbdaae5d4d1c6a83afe3573a8aea155b620
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-01 16:05:48 +0200

    feat: add a toaster layout for login page

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,16 @@
+import { Toaster } from "sonner";
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster position="top-center" richColors />
+      </body>
+    </html>
+  );
+}
