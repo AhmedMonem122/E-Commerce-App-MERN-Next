32c780a0e667d4fb99838e540067a975c3a2421b
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-02 12:35:35 +0200

    feat: add api client for client API requests

---
+++ src/api/apiClient.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token"); // 🍪 read cookie
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default apiClient;
