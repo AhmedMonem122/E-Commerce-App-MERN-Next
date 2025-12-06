a59ac6d5f84dd63f8e7bd718a172571e6729135d
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-06 02:23:31 +0200

    feat: add logout api route

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const res = NextResponse.json({ ok: true });
+
+  // remove token cookie
+  res.cookies.set({
+    name: "token",
+    value: "",
+    maxAge: 0,
+    path: "/",
+  });
+
+  return res;
+}
