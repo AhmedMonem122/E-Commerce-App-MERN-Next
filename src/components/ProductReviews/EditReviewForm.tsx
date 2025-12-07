79099c19b712b20bf31e2b1867ae279eb0467af2
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-07 12:04:44 +0200

    style: update edit review form styles

--- src/components/ProductReviews/EditReviewForm.tsx
+++ src/components/ProductReviews/EditReviewForm.tsx
@@ -22,43 +22,34 @@ const EditReviewForm = ({
   review: Review;
 }) => {
   const initialState: EditFormState = {};
-
-  const actionWithId = addOrEditReviewAction.bind(null, productId);
-  const [state, formAction] = useActionState(actionWithId, initialState);
+  const action = addOrEditReviewAction.bind(null, productId);
+  const [state, formAction] = useActionState(action, initialState);
 
   useEffect(() => {
-    // This triggers **after the form action updates state**
-    if (state?.success) toast.success(state.message);
+    if (state?.success) toast.success("Review updated!");
     if (state?.success === false) toast.error(state.message);
   }, [state]);
 
   return (
-    <form className="space-y-4" action={formAction}>
+    <form action={formAction} className="flex-1 space-y-3">
       <input type="hidden" name="reviewId" value={review._id} />
 
       <Input
         type="number"
-        name="rating"
         min={1}
         max={5}
+        name="rating"
         defaultValue={review.rating}
-        className="w-20"
       />
-      {state?.errors?.rating && (
-        <p className="text-red-500 text-sm">{state.errors.rating}</p>
-      )}
 
       <Textarea
         name="comment"
         defaultValue={review.review}
-        placeholder="Edit your review..."
+        className="min-h-[80px]"
       />
-      {state?.errors?.comment && (
-        <p className="text-red-500 text-sm">{state.errors.comment}</p>
-      )}
 
-      <Button type="submit" variant="outline" size="sm">
-        Update
+      <Button size="sm" variant="secondary">
+        Save
       </Button>
     </form>
   );
