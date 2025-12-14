89f21729d600dc5c4b47537ed5cbbf175eb71119
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-14 13:52:06 +0200

    feat: add profile layout

---
+++ src/app/profile/layout.tsx
@@ -0,0 +1,16 @@
+import { Toaster } from "sonner";
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster position="bottom-right" richColors />
+      </body>
+    </html>
+  );
+}
