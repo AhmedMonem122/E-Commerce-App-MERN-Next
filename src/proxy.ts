6baf08a9f5fb512755df7c7269bff18e35e01439
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-12-23 17:37:58 +0200

    feat!: update proxy middleware to protect the admin dashboard from user access && vice versa

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,36 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import apiServer from "@/app/lib/apiServer.server";
+import axios from "axios";
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (!token) {
-    const loginUrl = new URL("/login", request.url);
+  let user = null;
+  try {
+    const api = await apiServer();
+    const res = await api.get("/users/me"); // adjust if your endpoint differs
+    user = res.data?.data?.user ?? null;
+  } catch (axiosError) {
+    if (axios.isAxiosError(axiosError)) {
+      // not authenticated or error -> user stays null
+      user = null;
+    }
+  }
+
+  const loginUrl = new URL("/login", request.url);
+  if (!token && !user) {
+    loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (user?.role === "admin" && request.nextUrl.pathname.startsWith("/user")) {
+    loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  } else if (
+    user?.role === "user" &&
+    request.nextUrl.pathname.startsWith("/admin")
+  ) {
     loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
   }
