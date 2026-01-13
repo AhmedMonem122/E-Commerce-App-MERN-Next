853e1c37cbfea97fa9954ab4beb8fe032a7abbe3
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-01-14 00:04:48 +0200

    feat: add add product page

---
+++ src/app/(dashboard)/admin/dashboard/products/add/page.tsx
@@ -0,0 +1,62 @@
+import { addProductAction } from "@/app/actions/productActions/productActions";
+import { getProductFormData } from "@/app/lib/product-form-data";
+import { ActionForm } from "@/components/Dashboard/ReusableComponents/Forms/ActionForm";
+import { Brand } from "@/types/brand";
+import { Category } from "@/types/category";
+import { AddProductForm } from "@/types/product-form";
+
+export default async function AddProductPage() {
+  const { brands, categories } = await getProductFormData();
+
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h1 className="text-2xl font-semibold">Add Product</h1>
+
+      <ActionForm<AddProductForm>
+        action={addProductAction}
+        submitLabel="Create Product"
+        fields={[
+          { name: "title", label: "Title", type: "text" },
+          {
+            name: "description",
+            label: "Description",
+            type: "textarea",
+          },
+          {
+            name: "price",
+            label: "Price",
+            type: "number",
+          },
+          {
+            name: "brand",
+            label: "Brand",
+            type: "select",
+            options: brands.map((b: Brand) => ({
+              label: b.title,
+              value: b._id,
+            })),
+          },
+          {
+            name: "category",
+            label: "Category",
+            type: "select",
+            options: categories.map((c: Category) => ({
+              label: c.title,
+              value: c._id,
+            })),
+          },
+          {
+            name: "imageCover",
+            label: "Cover Image",
+            type: "file",
+          },
+          {
+            name: "images",
+            label: "Gallery Images",
+            type: "file",
+          },
+        ]}
+      />
+    </div>
+  );
+}
