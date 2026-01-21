a396f77948d7554f002c61287116f47edf5886db
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-01-22 01:35:40 +0200

    feat: add edit product layout

---
+++ src/app/(dashboard)/admin/dashboard/products/edit/[id]/layout.tsx
@@ -0,0 +1,14 @@
+import { Toaster } from "sonner";
+
+export default function EditProductLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <Toaster position="top-center" richColors />
+    </>
+  );
+}
