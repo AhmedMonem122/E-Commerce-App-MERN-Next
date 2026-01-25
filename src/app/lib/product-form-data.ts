7f6fc17f1297bbbb5916b16821947c64258dc371
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-01-25 17:00:50 +0200

    feat!: update product form data logic

--- src/app/lib/product-form-data.ts
+++ src/app/lib/product-form-data.ts
@@ -2,13 +2,15 @@ import apiServer from "./apiServer.server";
 
 export async function getProductFormData() {
   const api = await apiServer();
-  const [brandsRes, categoriesRes] = await Promise.all([
+  const [brandsRes, categoriesRes, productsRes] = await Promise.all([
     api.get("/brands"),
     api.get("/categories"),
+    api.get("/products"),
   ]);
 
   return {
     brands: brandsRes.data.data.brands,
     categories: categoriesRes.data.data.categories,
+    products: productsRes.data.data.products,
   };
 }
